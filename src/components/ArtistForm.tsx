fa1a76d96dd0e738da09031cd324d509cd89fa0f
Ivan Vashchuk <vaniavaschuk@gmail.com>
2025-09-22 16:20:43 +0900

    styles and select

--- src/components/ArtistForm.tsx
+++ src/components/ArtistForm.tsx
@@ -1,5 +1,13 @@
 "use client";
 import { useState } from "react";
+import React from "react";
+import Select from 'react-select'
+
+const options = [
+  { value: "Lotte World Seoul 2026", label: "Lotte World Seoul 2026" },
+  { value: "Lotte World Busan 2026", label: "Lotte World Busan 2026" },
+  { value: "Gyeongju World 2026", label: "Gyeongju World 2026" },
+]
 
 export default function ArtistForm() {
   const [loading, setLoading] = useState(false);
@@ -28,57 +36,59 @@ export default function ArtistForm() {
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-3 max-w-xl justify-center items-center w-full">
       <label className="text-m font-bold self-start">Select place:</label>
-      <select name="project" id="project">
-        <option value="Lotte World Seoul 2026">Lotte World Seoul 2026</option>
-        <option value="Lotte World Busan 2026">Lotte World Busan 2026</option>
-        <option value="Gyeongju World 2026">Gyeongju World 2026</option>
-      </select>
+      <Select name="place" options={options} className="w-full mb-5" styles={{
+        control: (BaseStyles, state) => ({
+          ...BaseStyles,
+          borderColor: state.isFocused ? "black" : "black",
+          borderRadius: "0",
+        })
+        }}/>
 
       <label className="text-m font-bold self-start">Full name:</label>
-      <input name="fullName" placeholder="Full name" required className="border p-2 rounded-xl w-full mb-5" />
+      <input name="fullName" placeholder="Full name" required className="bg-white border p-2 w-full mb-5" />
 
       <label className="text-m font-bold self-start">Email:</label>
-      <input name="email" type="email" placeholder="Email" required className="border p-2 rounded-xl w-full mb-5" />
+      <input name="email" type="email" placeholder="Email" required className="border p-2 bg-white w-full mb-5" />
 
       <label className="text-m font-bold self-start">Phone number:</label>
-      <input name="phone" placeholder="Phone" className="border p-2 rounded-xl w-full mb-5" />
+      <input name="phone" placeholder="Phone" className="border p-2 bg-white w-full mb-5" />
 
       <label className="text-m font-bold self-start">Nationality:</label>
-      <input name="country" placeholder="Country" className="border p-2 rounded-xl w-full mb-5" />
+      <input name="country" placeholder="Country" className="border p-2 bg-white w-full mb-5" />
 
       <label className="text-m font-bold self-start">Current country/city:</label>
-      <input name="city" placeholder="City" className="border p-2 rounded-xl w-full mb-5" />
+      <input name="city" placeholder="City" className="border p-2 bg-white w-full mb-5" />
 
       <label className="text-m font-bold self-start">Date of birth:</label>
-      <input name="dateOfBirth" type="date" placeholder="Date of birth" className="border p-2 rounded-xl w-full mb-5" />
+      <input name="dateOfBirth" type="date" placeholder="Date of birth" className="border p-2 bg-white w-full mb-5" />
 
       <label className="text-m font-bold self-start">Body shape:</label>
-      <input name="height" placeholder="Height (e.g. 175 cm)" className="border p-2 rounded-xl w-full" />
-      <input name="weight" placeholder="Weight (e.g. 62 kg)" className="border p-2 rounded-xl w-full" />
-      <input name="waist" placeholder="Waist (e.g. 70 cm)" className="border p-2 rounded-xl w-full" />
-      <input name="bust" placeholder="Bust (e.g. 90 cm)" className="border p-2 rounded-xl w-full mb-5" />
+      <input name="height" placeholder="Height (e.g. 175 cm)" className="border p-2 bg-white w-full" />
+      <input name="weight" placeholder="Weight (e.g. 62 kg)" className="border p-2 bg-white w-full" />
+      <input name="waist" placeholder="Waist (e.g. 70 cm)" className="border p-2 bg-white w-full" />
+      <input name="bust" placeholder="Bust (e.g. 90 cm)" className="border p-2 bg-white w-full mb-5" />
 
       <label className="text-m font-bold self-start">Instagram name:</label>
-      <input name="instagram" placeholder="Instagram" className="border p-2 rounded-xl w-full mb-5" />
+      <input name="instagram" placeholder="Instagram" className="border p-2 bg-white w-full mb-5" />
 
       <label className="text-m font-bold self-start">Position:</label>
-      <input name="position" placeholder="Dancer, acrobat, juggle etc." className="border p-2 rounded-xl w-full mb-5" />
+      <input name="position" placeholder="Dancer, acrobat, juggle etc." className="border p-2 bg-white w-full mb-5" />
 
       <label className="text-m font-bold self-start">Experience:</label>
       <p className="text-gray-500 self-start">In format: Place of work (date from - date to, city/country)</p>
-      <textarea name="experience" placeholder="Lotte World (2018-2019, Seoul), Chimelong (2021-2022, China)..." className="border p-2 rounded-xl w-full mb-5" rows={5} />
+      <textarea name="experience" placeholder="Lotte World (2018-2019, Seoul), Chimelong (2021-2022, China)..." className="border p-2 bg-white w-full mb-5" rows={5} />
 
       <label className="text-m font-bold self-start">Additional information:</label>
       <p className="text-gray-500 self-start">Here you can tell about your degree, awards or other additional skills you have</p>
-      <textarea name="additional" placeholder="Professional dancer with bachelor degree: Kyiv National University of Culture and Arts (2020-2024). Strong technique in modern and ballroom dancers. Skills in acrobatic, singing, acting etc....." className="border p-2 rounded-xl w-full mb-5" rows={5} />
+      <textarea name="additional" placeholder="Professional dancer with bachelor degree: Kyiv National University of Culture and Arts (2020-2024). Strong technique in modern and ballroom dancers. Skills in acrobatic, singing, acting etc....." className="border p-2 bg-white w-full mb-5" rows={5} />
 
       <label className="text-m font-bold self-start">Additional information:</label>
       <p className="text-gray-500 self-start">Here you can tell about your degree, awards or other additional skills yu have</p>
-      <textarea name="following" placeholder="You can add some other links (video, promo, pictures etc.) to present yourself better or add following text about you." className="border p-2 rounded-xl w-full mb-5" rows={5} />
+      <textarea name="following" placeholder="You can add some other links (video, promo, pictures etc.) to present yourself better or add following text about you." className="border p-2 bg-white w-full mb-5" rows={5} />
 
       <label className="text-m font-bold self-start">Photo (portrait / headshot)</label>
-      <input name="photo" type="file" accept="image/*" className="border p-2 rounded-xl w-full mb-5" />
-      <button type="submit" disabled={loading} className="bg-blue-700 text-white px-4 py-2 rounded-xl w-full cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed">
+      <input name="photo" type="file" accept="image/*" className="border p-2 bg-white w-full mb-5 cursor-pointer" />
+      <button type="submit" disabled={loading} className="bg-blue-700 text-white px-4 py-2 w-full cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed">
         {loading ? "Sending..." : "Submit"}
       </button>
       {status === "success" && <p className="text-green-600">✅ Sent successfully</p>}
