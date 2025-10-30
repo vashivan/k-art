089c24c9c2135387ab417bd493c2f660fba9aa4d
Ivan Vashchuk <vaniavaschuk@gmail.com>
2025-10-30 21:03:41 +0900

    &

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
-module.exports = nextConfig;
+
+export default nextConfig;
