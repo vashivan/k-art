ca202b350dc577526d7e3413ed6ba95f6c1dc01c
Ivan Vashchuk <vaniavaschuk@gmail.com>
2025-11-05 01:00:44 +0900

    Api, suspense

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,13 +1,16 @@
 import ArtistForm from "@/components/ArtistForm";
 import React from "react";
+import { Suspense } from "react";
 import Link from "next/link";
 
 export default function Page() {
   return (
     <main className="p-10 flex flex-col w-full justify-center items-center">
       <h1 className="text-2xl font-bold mb-10 text-center">Please, fill the form below to apply</h1>
-      <ArtistForm />
-        <Link href="/" className="mt-5 text-bold text-xl underline">Back to home</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ArtistForm />
+      </Suspense>
+      <Link href="/" className="mt-5 text-bold text-xl underline">Back to home</Link>
     </main>
   )
 }
