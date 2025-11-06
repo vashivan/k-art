512d8c1942f711a95bfa9aab70109356fd897751
Ivan Vashchuk <vaniavaschuk@gmail.com>
2025-11-06 15:33:58 +0900

    Pics

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,16 +1,16 @@
-import styles from "../../style/MainPage.module.scss";
+import styles from "../../style/SuccessPage.module.scss";
 import Link from "next/link";
 
-export default function Page() { 
-  return(
-        <div className={styles.container}>
+export default function Page() {
+  return (
+    <div className={styles.container}>
       <div className={styles.container__black}></div>
       <main className={styles.container__main}>
         <h2 className="text-3xl sm:text-5xl font-extrabold tracking-tight text-white mb-6">
           Thank you for submitting your application!
         </h2>
         <p className="max-w-2xl text-lg text-white font-bold mb-8">
-          Thank you — we’ve received your application.<br/>Our team will review it and contact you with the next steps. We appreciate your interest in working with K‑Art.
+          Thank you — we’ve received your application.<br />Our team will review it and contact you with the next steps. We appreciate your interest in working with K‑Art.
         </p>
         <Link
           href="/"
